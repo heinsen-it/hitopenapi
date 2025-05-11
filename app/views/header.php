c4b15402a3b09fc795696a7c5413b9d9d2a8ea46
Kjell Heinsen <ich@kjell-heinsen.de>
2025-05-11 02:02:15 +0200

    view Header

---
+++ app/views/header.php
@@ -0,0 +1,6 @@
+<?php
+// Headerdefinitionen für die API
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *'); // In Produktion einschränken!
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
